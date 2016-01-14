ff0213186daa564f309355136d5dbd8eefacbe2a
crisbern <cBernardis@DUBLT01928.esri-holdings.com>
2016-01-14 23:03:35 +0000

    Working select layers

--- widgets/80days/Widget.js
+++ widgets/80days/Widget.js
@@ -21,10 +21,10 @@ define(['dojo/_base/declare',
     'jimu/BaseWidget',
     'esri/TimeExtent',
     'esri/dijit/TimeSlider',
-    'dojo/_base/array',
-    'dojo/_base/lang','esri/request'
+    'dojo/_base/array','esri/layers/FeatureLayer',
+    'dojo/_base/lang', 'esri/request', 'esri/tasks/query', 'esri/tasks/StatisticDefinition', 'esri/tasks/QueryTask', "esri/symbols/SimpleMarkerSymbol", "esri/symbols/SimpleFillSymbol", "esri/Color"
 ],
-  function (declare, html, query, _WidgetsInTemplateMixin, BaseWidget, TimeExtent, TimeSlider, array, lang, esriRequest) {
+  function (declare, html, query, _WidgetsInTemplateMixin, BaseWidget, TimeExtent, TimeSlider, array, FeatureLayer, lang, esriRequest, Query, StatisticDefinition, QueryTask, SimpleMarkerSymbol, SimpleFillSymbol , Color) {
       var clazz = declare([BaseWidget, _WidgetsInTemplateMixin], {
 
           _hasContent: null,
@@ -32,14 +32,22 @@ define(['dojo/_base/declare',
           postCreate: function () {
               this.inherited(arguments);
 
+              var layerName = lang.getObject('stops_layer', false, this.config);
+              var operationalLayer = lang.getObject('map.itemInfo.itemData.operationalLayers', false, this);
+              this.stops_layer = new FeatureLayer(this._getLayer(operationalLayer, layerName).url, { mode: FeatureLayer.MODE_SELECTION });
+              this.stops_layer.setRenderer(this._getLayer(operationalLayer, layerName).layerObject.renderer);
+              this.map.addLayer(this.stops_layer);
+              
+              var layerName = lang.getObject('path_layer', false, this.config);
+              this.path_layer = new FeatureLayer(this._getLayer(operationalLayer, layerName).url, { mode: FeatureLayer.MODE_SELECTION });
+              this.path_layer.setRenderer(this._getLayer(operationalLayer, layerName).layerObject.renderer);
+              this.map.addLayer(this.path_layer);
           },
 
           startup: function () {
               this.inherited(arguments);
                          
               // create time slider
-              
-
               var tableName = lang.getObject('distance_table', false, this.config);
               var table = lang.getObject('map.itemInfo.itemData.tables', false, this);
 
@@ -56,18 +64,45 @@ define(['dojo/_base/declare',
                   var extentObj = lang.getObject('timeInfo.timeExtent', false, result);
                   var extent = new TimeExtent(new Date(extentObj[0]), new Date(extentObj[1]));
                   timeSlider.createTimeStopsByTimeInterval(extent, 1, 'esriTimeUnitsDays');
-                  timeSlider.on("time-extent-change", this.extentChanged);
+                  timeSlider.on("time-extent-change", lang.hitch(this, this.extentChanged));
                   timeSlider.startup();
 
               }));
 
+
               
 
           },
-          extentChanged: function (evt) {
-              var startVal = evt.startTime.getFullYear();
-              var endVal = evt.endTime.getFullYear();
-              console.debug("<b> Current Time Extent:" + startVal + " TO " + endVal + "<\/b>");
+          extentChanged: function (time) {
+
+              var tableName = lang.getObject('distance_table', false, this.config);
+              var table = lang.getObject('map.itemInfo.itemData.tables', false, this);
+
+              var q = new Query();
+              q.timeExtent = time;
+
+              var statisticDefinition = new StatisticDefinition();
+              statisticDefinition.statisticType = "sum";
+              statisticDefinition.onStatisticField = "km";
+              statisticDefinition.outStatisticFieldName = "TotalKm";
+
+              q.outStatistics = [statisticDefinition];
+
+              var qT = new QueryTask(this._getLayer(table, tableName).url);
+              qT.execute(q)
+              .then(lang.hitch(this, function(totalF){
+                  var total = lang.getObject('attributes.TotalKm', false, totalF.features[0]);
+                  
+                  var q = new Query();
+                  q.where = 'travel_distance <=' + total*1000;
+                  var layerName = lang.getObject('stops_layer', false, this.config);
+                  var operationalLayer = lang.getObject('map.itemInfo.itemData.operationalLayers', false, this);
+
+                  this.stops_layer.selectFeatures(q)
+                  this.path_layer.selectFeatures(q)
+              }));
+            
+              
               /*
 2) query total km extent
 setSelectionSymbol(symbol) is its symbol with alpha = 0
